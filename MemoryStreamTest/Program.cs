47d7f2005e0b34f49225507ae2a65c77cd15996d
Bartek <bartlomiej.kacki@gmail.com>
2024-08-20 19:22:58 +0200

    Add project files.

---
+++ MemoryStreamTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MemoryStreamTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var memoryStream = new MemoryStream())
+            {
+                using(var streamWriter =  new StreamWriter(memoryStream))
+                {
+                    streamWriter.WriteLine("The value is {0:0.00}", 123.45678);
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(memoryStream.ToArray()));
+            }
+        }
+    }
+}
